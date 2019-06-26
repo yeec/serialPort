0d88f366758418a001f0bb028ba4b0ae52a9d393
yeec <wyc3247@163.com>
2019-06-26 15:47:05 +0800

    修改数据库连接字符串

--- manage/src/main/java/bros/manage/util/JDBCUtil.java
+++ manage/src/main/java/bros/manage/util/JDBCUtil.java
@@ -13,9 +13,10 @@ public class JDBCUtil {
  
     public static boolean isValidateConnection(String ip,String port,String svrName,String user,String pass) throws Exception {
     	try{
-    		String URL="jdbc:oracle:thin:@"+ip+":"+port+":"+svrName;
+//    		String URL="jdbc:oracle:thin:@"+ip+":"+port+":"+svrName;
+    		String URL = "jdbc:oracle:thin:@(description=(address_list=(address=(protocol=tcp)(port="+port+")(host="+ip+")))(connect_data=(service_name="+svrName+")))";
     		//1.加载驱动程序
-    		Class.forName("oracle.jdbc.driver.OracleDriver");
+    		Class.forName("oracle.jdbc.OracleDriver");
     		//2.获得数据库链接
     		DriverManager.setLoginTimeout(3);
     		Connection conn=DriverManager.getConnection(URL, user, pass);
